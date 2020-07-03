be27378663675f68b159135cb2f92a9d4034be08
Kim Yujeong <dbwjd201166@gmail.com>
2020-07-03 19:30:25 +0900

    Add Floating Action Button on Color Selection Activity

--- app/src/main/java/com/example/colorfuldays/ColorSelectionActivity.java
+++ app/src/main/java/com/example/colorfuldays/ColorSelectionActivity.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.colorfuldays.veiwlist.Adapter;
 import com.example.colorfuldays.veiwlist.VerticalSpaceItemDecoration;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -25,6 +27,15 @@ public class ColorSelectionActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_selection);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         init();
     }
