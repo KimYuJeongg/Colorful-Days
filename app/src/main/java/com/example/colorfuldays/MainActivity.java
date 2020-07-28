8240b43578b14ab0ca2fdfeb2db1eea972809046
Kim Yujeong <dbwjd201166@gmail.com>
2020-07-28 14:56:49 +0900

    Refactor add buttons(TableLayout -> GridLayout)

--- app/src/main/java/com/example/colorfuldays/MainActivity.java
+++ app/src/main/java/com/example/colorfuldays/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.colorfuldays;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -16,11 +23,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
 {
     private AppBarConfiguration mAppBarConfiguration;
+    public static final int GET_COLOR_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -46,7 +55,29 @@ public class MainActivity extends AppCompatActivity
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                switch (item.getItemId())
+                {
+                    case R.id.navigation_home:
+                    case R.id.navigation_menu:
+                        break;
+                    case R.id.navigation_add:
+                        Intent intent = new Intent(getApplicationContext(), ColorSelectionActivity.class);
+                        startActivityForResult(intent, GET_COLOR_REQUEST);
+                        break;
+                }
+                return false;
+            }
+        });
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -63,18 +94,52 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        else
             super.onBackPressed();
-        }
     }
 
     public void onColorButtonClick(View v)
     {
         Intent intent = new Intent(getApplicationContext(), ColorSelectionActivity.class);
-        startActivity(intent);
+
+        startActivityForResult(intent, GET_COLOR_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent)
+    {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == GET_COLOR_REQUEST)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                String name = intent.getStringExtra("name");
+
+                GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+                int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+                Button button = new Button(this);
+                GridLayout.LayoutParams layoutParams=new GridLayout.LayoutParams();
+
+                layoutParams.setMargins(27,30,0,0);
+                layoutParams.width=size;
+                layoutParams.height=size;
+                button.setLayoutParams(layoutParams);
+                button.setBackgroundResource(R.drawable.color_button_shape);
+
+                GradientDrawable dynamicColor = (GradientDrawable) button.getBackground().getCurrent();
+                dynamicColor.setColor(Color.parseColor(name));
+
+                gridLayout.addView(button);
+
+                Toast.makeText(getApplicationContext(), "Received : " + name, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
