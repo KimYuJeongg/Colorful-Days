0feeb627f35208f0102acc9c2f8766248b2f3431
Kim Yujeong <dbwjd201166@gmail.com>
2020-09-28 22:12:34 +0900

    Add quote function

--- app/src/main/java/com/example/colorfuldays/DiaryActivity.java
+++ app/src/main/java/com/example/colorfuldays/DiaryActivity.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
 import java.io.InputStream;
 
 public class DiaryActivity extends AppCompatActivity
@@ -28,23 +29,31 @@ public class DiaryActivity extends AppCompatActivity
         @Override
         public void onClick(View view)
         {
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
             switch (view.getId())
             {
                 case R.id.insert_image:
-                    Toast.makeText(getApplicationContext(), "Pushed ", Toast.LENGTH_LONG).show();
                     Intent intent = new Intent();
                     intent.setType("image/*");
                     intent.setAction(Intent.ACTION_GET_CONTENT);
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                     startActivityForResult(intent, 1);
                     break;
                 case R.id.more:
-                    Toast.makeText(getApplicationContext(), "rr Pushed ", Toast.LENGTH_LONG).show();
+                    ImageView more = new ImageView(DiaryActivity.this);
+                    more.setBackgroundResource(R.drawable.ic_more);
+                    linearLayout.addView(more);
                     break;
                 case R.id.quote:
+                    ImageView quote = new ImageView(DiaryActivity.this);
+                    quote.setBackgroundResource(R.drawable.ic_quote);
+                    linearLayout.addView(quote);
+                    break;
                 case R.id.bold:
                     EditText editText = (EditText) findViewById(R.id.edit_content);
                     Spannable spannable = (Spannable) editText.getText();
                     spannable.setSpan(new StyleSpan(Typeface.BOLD), editText.getSelectionStart(), editText.getSelectionEnd(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    //볼드 해제 추가
                     break;
                 case R.id.align:
                 case R.id.size:
@@ -93,8 +102,8 @@ public class DiaryActivity extends AppCompatActivity
 
                     LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
                     ImageView imageView = new ImageView(DiaryActivity.this);
-                    linearLayout.addView(imageView);
                     imageView.setImageBitmap(img);
+                    linearLayout.addView(imageView);
                 } catch (Exception e)
                 {
 
@@ -106,4 +115,14 @@ public class DiaryActivity extends AppCompatActivity
             }
         }
     }
+
+    private BitmapFactory.Options getBitmapSize(File imageFile)
+    {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        return options;
+    }
+
 }
